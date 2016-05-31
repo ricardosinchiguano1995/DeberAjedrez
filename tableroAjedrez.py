ae98798c84fd3c905a09b7a97b24d589abf09d57
ricardosinchiguano1995 <rikysdjs1995@hotmail.es>
2016-05-31 02:16:40 -0500

    II
    Formando los tableros con dos for solo hace falta poner colores

--- tableroAjedrez.py
+++ tableroAjedrez.py
@@ -1,1 +1,11 @@
 # PROGRAMA QUE SIMULA UN TABLERO DE AJEDREZ
+from tkinter import *
+ventana=Tk()
+ventana.title("TABLERO AJEDREZ")
+canva=Canvas(ventana, width=500 ,height=500)
+canva.pack()
+for i in range (0,10):
+	for j in range(0,10):
+		canva.create_rectangle((20+(i*30),(50+(j*30)),30, 30)
+ventana.mainloop()
+
